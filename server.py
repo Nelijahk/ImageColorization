9667158f9acb208175101a3891a3fd90553643c2
Ilona <ilona.klymenok.knm.2020@lpnu.ua>
2023-05-30 00:20:29 +0300

    fix img output

--- server.py
+++ server.py
@@ -1,6 +1,3 @@
-import base64
-import io
-
 import cv2
 import numpy as np
 import os
@@ -9,11 +6,13 @@ from flask import Flask, render_template, request, send_file, jsonify
 from keras.models import load_model
 
 # Завантаження моделі для колоризації
-model = load_model('model')
+model = load_model('train_model(2)')
+
+save_dir = 'colorized_images'
+os.makedirs(save_dir, exist_ok=True)
 
 # Ініціалізація Flask додатку
 app = Flask(__name__)
-
 
 # Головна сторінка
 @app.route('/')
@@ -30,49 +29,39 @@ def colorize():
     # Завантажуємо чорнобіле зображення і перетворюємо його у RGB-режим
     img = Image.open(file)
     img = img.convert('RGB')
-
+    #
     img = np.array(img)
-    img = img.astype(np.uint8)
     img = cv2.resize(img, (160, 160))
+    img = img.astype('float32') / 255.0
 
     # Виконуємо операції для кольоризації зображення
     img = np.expand_dims(img, axis=0)
     pred_img = model.predict(img)
 
-    # Відновлюємо масштаб та перетворюємо на правильний діапазон значень
-    pred_img = (pred_img * 127.5) + 127.5
-    pred_img = np.clip(pred_img[0], 0, 255).astype(np.uint8)
+    pred_img = pred_img.reshape(160, 160, 3)
+    pred_img = cv2.resize(pred_img, (320, 320), interpolation=cv2.INTER_LANCZOS4)
 
-    # Зберігаємо кольорове зображення у файл
-    colorized_image_path = 'colorized_image.jpg'
-    pred_img_rgb = cv2.cvtColor(pred_img[0], cv2.COLOR_BGR2RGB)
-    colorized_img = Image.fromarray(pred_img_rgb)
-    colorized_img.save(colorized_image_path, format='JPEG')
+    # Масштабування значень пікселів до діапазону 0-255
+    predicted_img = cv2.convertScaleAbs(pred_img, alpha=(255.0 / pred_img.max()))
 
-    # Зберігаємо кольорове зображення у форматі BytesIO
-    output = io.BytesIO()
-    colorized_img = Image.fromarray(pred_img[0].astype(np.uint8))
-    colorized_img.save(output, format='JPEG')
-    output.seek(0)
+    # Конвертація зображення в об'єкт Image
+    pil_image = Image.fromarray(predicted_img)
 
-    # Перетворюємо BytesIO на рядок base64
-    base64_image = base64.b64encode(output.getvalue()).decode('utf-8')
+    # Збереження колоризованого зображення
+    save_path = os.path.join(save_dir, f'colorized_image.png')
+    pil_image.save(save_path)
 
-    # Повертаємо посилання на кольорове зображення та саме зображення у відповіді
-    colorized_image_url = f'{request.host_url}colorized_image.jpg'
-    # return jsonify({'image_url': colorized_image_url, 'image': base64_image})
     return render_template('index.html')
-
 
 # Маршрут для отримання кольоризованого зображення
 @app.route('/colorized_image.jpg')
 def get_colorized_image():
     # Отримуємо шлях до кольорового зображення
-    colorized_image_path = 'colorized_image.jpg'
+    colorized_image_path = 'colorized_images/colorized_image.png'
 
     # Перевіряємо, чи існує файл
     if os.path.exists(colorized_image_path):
-        return send_file(colorized_image_path, mimetype='image/jpeg')
+        return send_file(colorized_image_path, mimetype='image/png')
     else:
         return jsonify({'error': 'Colorized image not found'})
 
